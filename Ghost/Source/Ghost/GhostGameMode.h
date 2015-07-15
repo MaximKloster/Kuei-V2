c6ffa628835c4ab7f5744741e6618a72fc3aab13
MaximKloster <kloster.maxim@web.de>
2015-07-15 12:49:21 +0200

    Basic Setup

---
+++ Ghost/Source/Ghost/GhostGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "GhostGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GHOST_API AGhostGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
